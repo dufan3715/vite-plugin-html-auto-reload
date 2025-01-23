4d29df75873608ab31553e767f8899330bf68351
dufan <dufan3715@163.com>
2025-01-23 15:25:05 +0800

    feat: v1.0.12版本更新
    1. 事件终止调整为使用AbortController

--- src/index.ts
+++ src/index.ts
@@ -57,75 +57,77 @@ const getScriptChildren = (
   } = option;
   const ms = typeof polling === 'number' ? polling : 1000 * 60;
   const funcStr = `
-      /* eslint-disable */
-      const localVersion = '${version}';
-      const key = \`__html_auto_reload_\${localVersion}__\`;
-      let confirmed = sessionStorage.getItem(key) === 'Y';
-      ${polling ? `let timer;` : ''}
-      const checkVersion = () => {
-        if (confirmed) return;
-        const url = \`${versionUrl}?t=\${Date.now()}\`;
-        fetch(url)
-          .then(res => res.text())
-          .then(remoteVersion => {
-            if (confirmed) return;
-            if (remoteVersion && remoteVersion.length === localVersion.length && remoteVersion !== localVersion) {
-              ${once ? `confirmed = true;` : ''}
-              sessionStorage.setItem(key, 'Y')
-              if (window.confirm('${promptContent}')) {
-                window.location.reload();
-              } ${once ? `else {
-                removeEvent();
-              }` : ''}
-            }
-          })
-      };
-      ${
-        onerror
-          ? `function errorListener(event) {
-        const error = event.reason || event;
-        const source = event.target || event.srcElement;
-        if (error.message?.includes('Loading chunk') ||
-            source instanceof HTMLScriptElement ||
-            source instanceof HTMLLinkElement) {
-          checkVersion();
-        }
-      }`
-          : ''
+    /* eslint-disable */
+    const localVersion = '${version}';
+    const key = \`__html_auto_reload_\${localVersion}__\`;
+    let confirmed = sessionStorage.getItem(key) === 'Y';
+    ${polling ? `let timer;` : ''}
+    const checkVersion = () => {
+      if (confirmed) return;
+      const url = \`${versionUrl}?t=\${Date.now()}\`;
+      fetch(url)
+        .then(res => res.text())
+        .then(remoteVersion => {
+          if (confirmed) return;
+          if (
+            remoteVersion &&
+            remoteVersion.length === localVersion.length &&
+            remoteVersion !== localVersion
+          ) {
+            ${once ? `confirmed = true;` : ''}
+            sessionStorage.setItem(key, 'Y')
+            if (window.confirm('${promptContent}')) {
+              window.location.reload();
+            } ${once ? `else {
+              removeEvent();
+            }` : ''}
+          }
+        })
+    };
+    ${onerror ? `
+    function errorListener(event) {
+      const error = event.reason || event;
+      const source = event.target || event.srcElement;
+      if (
+        error.message?.includes('Loading chunk') ||
+        source instanceof HTMLScriptElement ||
+        source instanceof HTMLLinkElement
+      ) {
+        checkVersion();
       }
-      function removeEvent() {
-        ${
-          onerror
-            ? `window.removeEventListener('error', errorListener, true);`
-            : ''
-        }
-        ${polling ? `window.clearInterval(timer);` : ''}
-      };
-      function addEvent() {
-        ${
-          onvisibilitychange
-            ? `document.addEventListener('visibilitychange', () => {
+    }`
+    : ''}
+    const controller = new AbortController();
+    function removeEvent() {
+      controller.abort();
+      ${polling ? `window.clearInterval(timer);` : ''}
+    };
+    function addEvent() {
+      ${onvisibilitychange ? `
+      document.addEventListener(
+        'visibilitychange',
+        () => {
           if (document.hidden) return;
           checkVersion();
-        });`
-            : ''
-        }
-        ${
-          onerror
-            ? `window.addEventListener('error', errorListener, true);`
-            : ''
-        }
-        ${
-          polling
-            ? `timer = setInterval(() => {
-            if (document.hidden) return;
-            checkVersion();
-          }, ${ms});`
-            : ''
-        }
-      };
-      addEvent();
-    `.replace(/^\s*[\r\n]/gm, '');
+        },
+        { signal: controller.signal }
+      );`
+    : ''}
+      ${onerror ? `
+      window.addEventListener('error', errorListener, {
+        capture: true,
+        signal: controller.signal,
+      });`
+    : ''}
+      ${polling ? `
+      timer = setInterval(() => {
+        if (document.hidden) return;
+        checkVersion();
+      }, ${ms});`
+    : ''}
+    };
+    addEvent();
+  `.replace(/^\s*[\r\n]/gm, '');
   return `\n${funcStr}`;
 };
 
@@ -138,14 +140,12 @@ const htmlAutoReload = (option: HtmlAutoReloadOption = {}): PluginOption => {
       config = resolvedConfig;
     },
     transformIndexHtml() {
-      return [
-        {
-          tag: 'script',
-          attrs: { type: 'module', defer: true },
-          children: getScriptChildren(config, option),
-          injectTo: 'body',
-        },
-      ];
+      return [{
+        tag: 'script',
+        attrs: { type: 'module' },
+        children: getScriptChildren(config, option),
+        injectTo: 'body',
+      }]
     },
     closeBundle() {
       const outputDir = path.resolve(config.build.outDir);
